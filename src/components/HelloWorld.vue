472528f0dc4ccff3bf89fa04ae854913e4ae02b2
Julissamigdalia <julissamigdaliaj@gmail.com>
2025-03-23 20:14:17 -0600

    Guardando archivos no rastreados

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,82 +1,30 @@
 <script setup>
-//metodos
+import Header from './Header.vue';
+import Services from './Services.vue';
+import LoginButtons from './LoginButtons.vue';
+import Footer from './Footer.vue';
+
 </script>
 
 <template>
-  <!-- estructura html -->
   <div class="container">
-
-    <h1 class="text-center">MASCOTAS </h1>
-    <!-- INICIO DEL PRIMER ROW -->
-    <div class="row">
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-header">
-            <h6>Gatos</h6>
-          </div>
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
-              content.</p>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card">
-          <div class="card-header">
-            <h6>Perros</h6>
-          </div>
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
-              content.</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- CIERRE DEL PRIMER ROW -->
-    <!-- INICIO DEL 2 ROW -->
-     <div class="row mt-4">
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png" 
-          class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
-              content.</p>
-
-            <button class="btn btn-warning">Más Información</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png" 
-          class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
-              content.</p>
-
-            <button class="btn btn-warning">Más Información</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-4">
-        <div class="card" style="width: 18rem;">
-          <img src="https://nupec.com/wp-content/uploads/2021/02/Captura-de-pantalla-2021-02-08-a-las-13.59.48.png" 
-          class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's 
-              content.</p>
-
-            <button class="btn btn-warning">Más Información</button>
-          </div>
-        </div>
-      </div>
-     </div>
-    <!-- CIERRE DEL 2 ROW -->
+    <Header />
+    <Services />
+    <p><strong>Consulta precios, servicios disponibles y encuentra nuestra ubicación.</strong></p>
+    <p><strong>Deja que tu auto brille con Destello Móvil</strong></p>
+    <LoginButtons />
+    <Footer />
   </div>
-
 </template>
 
 <style scoped>
-/* estilos css */
+.container {
+  background: white;
+  padding: 20px;
+  margin: 20px auto;
+  max-width: 600px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
 </style>
---
+++ src/components/Horario.vue
@@ -0,0 +1,46 @@
+<template>
+
+    <div class="container">
+        <h3 class="mt-4">Horatio de clases de Sistemas</h3>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Horario</th>
+                    <th scope="col">Lunes</th>
+                    <th scope="col">Martes</th>
+                    <th scope="col">Miercoles</th>
+                    <th scope="col">Jueves</th>
+                    <th scope="col">Viernes</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(h, index) in horario" :key="index">
+                <td>{{ h.hora }}</td>
+            </tr>
+
+
+
+            </tbody>
+        </table>
+    </div>
+
+</template>
+<script setup>
+
+const Horario = ref([
+    { hora: '07:00 - 09:00', lunes: 'Inteligencia Artificial (ACS - Vasquez Martínez Josué)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '09:00 - 11:00', lunes: 'Administracion De Redes (RDS - Garcia Llamandez Astrid)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '11:00 - 13:00', lunes: 'Desarrollo En La Nube (RDS - Perez García Auriel)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '13:00 - 15:00', lunes: 'Desarrollo Con Realidad Aumentada (LC3 - Deman)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '15:00 - 17:00', lunes: 'Desarrollo Con Realidad Aumentada (LDS - Deman)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '17:00 - 19:00', lunes: 'Desarrollo Agil (ACS - Vasquez Martínez Josué Israel / Grunov)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '19:00 - 21:00', lunes: 'Administracion De Redes (RDS - Garcia Llamandez Astrid)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '21:00 - 23:00', lunes: 'Inteligencia Artificial (MAGS - Vasquez Martínez Josué)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '23:00 - 01:00', lunes: 'Desarrollo Con Realidad Aumentada (ACS - Deman)', martes: '', miercoles: '', jueves: '', viernes: '' },
+    { hora: '10:00 - 11:00', lunes: '', martes: 'Desarrollo Agil (ACS - Vasquez Martínez Josué Israel / Grunov)', miercoles: '', jueves: '', viernes: '' },
+    { hora: '11:00 - 12:00', lunes: '', martes: 'Desarrollo En La Nube (4 - Perez García Auriel Delande)', miercoles: '', jueves: '', viernes: '' },
+    { hora: '14:00 - 16:00', lunes: '', martes: 'Taller De Investigacion II (9 - Antonio Jimenez Saltea / Grunov)', miercoles: '', jueves: '', viernes: '' }
+]);
+
+
+</script>
