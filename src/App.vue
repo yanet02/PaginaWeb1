d1fbaf1ac1d4f8447ef7c3f9051171e4b17891ff
yanet-02 <jenamoradagarcia@gmail.com>
2025-03-24 18:56:17 -0600

    Página de Componentes Yanet

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+// import Recorrido from './components/Recorrido.vue'
+// import Horario from './components/Horario.vue'
 </script>
 
 <template>
-
+    <!-- <Horario /> -->
   <HelloWorld msg="Vite + Vue" />
+  <!-- <Recorrido/> -->
+
 </template>
 
 <style scoped></style>
